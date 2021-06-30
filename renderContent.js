a5d2362d73c1e317c25ba52d01b6ffb48913da6e
Ryan <ryanjay333@gmail.com>
2021-06-30 14:34:12 -0400

    removed parsing of dates, added models to webinar object

--- renderContent.js
+++ renderContent.js
@@ -3,96 +3,54 @@ let htmlMarkup = '';
 
 const webinars = [
   {
-    img: 'https://marketing-image-production.s3.amazonaws.com/uploads/523460e6cd6435290b821731c4b604ad15d94bee161ad919dc521778ce5287cea315d79b24efb8585331359fc369fa4800c0b176c977e4f0f6bb13eafffa35ad.png',
-    name: 'LG Front Load Washer WM3570H*A/WM3770H*A',
+    img: 'https://www.myapstore.com/MarketingObjectRetrieval/Dispatcher?RequestType=Image&Name=CGI10865.jpg&Variant=ViewLarger',
+    name: 'GE Front Load Washer',
     language: 'English',
     host: 'Rick Kuemin',
-    description: `Diagnostics and repair of the WM3570H*A/WM3770H*A LG Front Load
-        Washer`,
-    dates: [
-      'Wednesday, March 24th, 8:30am - 10:30am',
-      'Thursday, March 25th, 5:00pm - 7:00pm',
+    description: `Diagnostics and repair of various GE front load washers`,
+    models: [
+      'GFWS2500',
+      'GFWS2505',
+      'GFWS2600',
+      'GFWS2605',
+      'GHWS3600',
+      'GHWS3605',
+      'GFWS3700',
+      'GFWS3705',
+      'GFWR4800',
+      'GFWR4805',
     ],
-    registrationURL: 'https://register.gotowebinar.com/rt/1942609860780894475',
+    date: 'Wednesday, July 21st, 5:00pm - 7:00pm',
+    registrationURL: '#',
   },
   {
-    img: 'https://marketing-image-production.s3.amazonaws.com/uploads/523460e6cd6435290b821731c4b604ad15d94bee161ad919dc521778ce5287cea315d79b24efb8585331359fc369fa4800c0b176c977e4f0f6bb13eafffa35ad.png',
-    name: 'LG Front Load Washer WM3570H*A/WM3770H*A',
+    img: 'https://www.myapstore.com/MarketingObjectRetrieval/Dispatcher?RequestType=Image&Name=CGI10865.jpg&Variant=ViewLarger',
+    name: 'GE Front Load Washer',
     language: 'English',
     host: 'Rick Kuemin',
-    description: `Diagnostics and repair of the WM3570H*A/WM3770H*A LG Front Load
-        Washer`,
-    dates: [
-      'Wednesday, March 24th, 8:30am - 10:30am',
-      'Thursday, March 25th, 5:00pm - 7:00pm',
+    description: `Diagnostics and repair of various GE front load washers`,
+    models: [
+      'GFWS2500',
+      'GFWS2505',
+      'GFWS2600',
+      'GFWS2605',
+      'GHWS3600',
+      'GHWS3605',
+      'GFWS3700',
+      'GFWS3705',
+      'GFWR4800',
+      'GFWR4805',
     ],
-    registrationURL: 'https://register.gotowebinar.com/rt/1942609860780894475',
-  },
-  {
-    img: 'https://marketing-image-production.s3.amazonaws.com/uploads/523460e6cd6435290b821731c4b604ad15d94bee161ad919dc521778ce5287cea315d79b24efb8585331359fc369fa4800c0b176c977e4f0f6bb13eafffa35ad.png',
-    name: 'LG Front Load Washer WM3570H*A/WM3770H*A',
-    language: 'English',
-    host: 'Rick Kuemin',
-    description: `Diagnostics and repair of the WM3570H*A/WM3770H*A LG Front Load
-        Washer`,
-    dates: [
-      'Wednesday, March 24th, 8:30am - 10:30am',
-      'Thursday, March 25th, 5:00pm - 7:00pm',
-    ],
-    registrationURL: 'https://register.gotowebinar.com/rt/1942609860780894475',
-  },
-  {
-    img: 'https://marketing-image-production.s3.amazonaws.com/uploads/523460e6cd6435290b821731c4b604ad15d94bee161ad919dc521778ce5287cea315d79b24efb8585331359fc369fa4800c0b176c977e4f0f6bb13eafffa35ad.png',
-    name: 'LG Front Load Washer WM3570H*A/WM3770H*A',
-    language: 'English',
-    host: 'Rick Kuemin',
-    description: `Diagnostics and repair of the WM3570H*A/WM3770H*A LG Front Load
-        Washer`,
-    dates: [
-      'Wednesday, March 24th, 8:30am - 10:30am',
-      'Thursday, March 25th, 5:00pm - 7:00pm',
-    ],
-    registrationURL: 'https://register.gotowebinar.com/rt/1942609860780894475',
-  },
-  {
-    img: 'https://marketing-image-production.s3.amazonaws.com/uploads/523460e6cd6435290b821731c4b604ad15d94bee161ad919dc521778ce5287cea315d79b24efb8585331359fc369fa4800c0b176c977e4f0f6bb13eafffa35ad.png',
-    name: 'LG Front Load Washer WM3570H*A/WM3770H*A',
-    language: 'English',
-    host: 'Rick Kuemin',
-    description: `Diagnostics and repair of the WM3570H*A/WM3770H*A LG Front Load
-        Washer`,
-    dates: [
-      'Wednesday, March 24th, 8:30am - 10:30am',
-      'Thursday, March 25th, 5:00pm - 7:00pm',
-    ],
-    registrationURL: 'https://register.gotowebinar.com/rt/1942609860780894475',
-  },
-  {
-    img: 'https://marketing-image-production.s3.amazonaws.com/uploads/523460e6cd6435290b821731c4b604ad15d94bee161ad919dc521778ce5287cea315d79b24efb8585331359fc369fa4800c0b176c977e4f0f6bb13eafffa35ad.png',
-    name: 'LG Front Load Washer WM3570H*A/WM3770H*A',
-    language: 'English',
-    host: 'Rick Kuemin',
-    description: `Diagnostics and repair of the WM3570H*A/WM3770H*A LG Front Load
-        Washer`,
-    dates: [
-      'Wednesday, March 24th, 8:30am - 10:30am',
-      'Thursday, March 25th, 5:00pm - 7:00pm',
-    ],
-    registrationURL: 'https://register.gotowebinar.com/rt/1942609860780894475',
+    date: 'Thursday, July 22nd, 8:30am - 10:30am',
+    registrationURL: '#',
   },
 ];
 
 function renderContent(webinar) {
-  // Parse array of dates
-  let dates = '';
-
-  webinar.dates.forEach(
-    (date) =>
-      (dates += `<p class="webinar-time">
-  <span class="webinar-time__icon"
-    ><i class="bi bi-clock-fill"></i></span
-  >${date}
-</p>`)
+  // Parse array of models
+  let models = '';
+  webinar.models.forEach(
+    (model) => (models += `<p class="webinar-model">${model}<p/>`)
   );
 
   // Create webinar component
@@ -121,10 +79,17 @@ function renderContent(webinar) {
       <p class="webinar-description">
         ${webinar.description}
       </p>
+      <div class="webinar-models">
+        ${models}
+      </div>
         <div class="webinar-dates__container">
         <div class="webinar-dates__header bold">Session Times (EST)</div>
         <div class="webinar-dates">
-            ${dates}
+        <p class="webinar-time">
+        <span class="webinar-time__icon"
+          ><i class="bi bi-clock-fill"></i></span
+        >${webinar.date}
+      </p>
         </div>
       </div>
       <a
