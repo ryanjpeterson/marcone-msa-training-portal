9d5fa2bb4e12e275223cfdce40216803c011bcea
Ryan <ryanjay333@gmail.com>
2021-06-30 11:54:55 -0400

    revised webinar-dates styles

--- renderContent.js
+++ renderContent.js
@@ -10,8 +10,8 @@ const webinars = [
     description: `Diagnostics and repair of the WM3570H*A/WM3770H*A LG Front Load
         Washer`,
     dates: [
-      'Wednesday, March 24th, 8:30am - 10:30am EST',
-      'Wednesday, March 24th, 8:30am - 10:30am EST',
+      'Wednesday, March 24th, 8:30am - 10:30am',
+      'Thursday, March 25th, 5:00pm - 7:00pm',
     ],
     registrationURL: 'https://register.gotowebinar.com/rt/1942609860780894475',
   },
@@ -23,8 +23,8 @@ const webinars = [
     description: `Diagnostics and repair of the WM3570H*A/WM3770H*A LG Front Load
         Washer`,
     dates: [
-      'Wednesday, March 24th, 8:30am - 10:30am EST',
-      'Wednesday, March 24th, 8:30am - 10:30am EST',
+      'Wednesday, March 24th, 8:30am - 10:30am',
+      'Thursday, March 25th, 5:00pm - 7:00pm',
     ],
     registrationURL: 'https://register.gotowebinar.com/rt/1942609860780894475',
   },
@@ -36,8 +36,8 @@ const webinars = [
     description: `Diagnostics and repair of the WM3570H*A/WM3770H*A LG Front Load
         Washer`,
     dates: [
-      'Wednesday, March 24th, 8:30am - 10:30am EST',
-      'Wednesday, March 24th, 8:30am - 10:30am EST',
+      'Wednesday, March 24th, 8:30am - 10:30am',
+      'Thursday, March 25th, 5:00pm - 7:00pm',
     ],
     registrationURL: 'https://register.gotowebinar.com/rt/1942609860780894475',
   },
@@ -49,8 +49,8 @@ const webinars = [
     description: `Diagnostics and repair of the WM3570H*A/WM3770H*A LG Front Load
         Washer`,
     dates: [
-      'Wednesday, March 24th, 8:30am - 10:30am EST',
-      'Wednesday, March 24th, 8:30am - 10:30am EST',
+      'Wednesday, March 24th, 8:30am - 10:30am',
+      'Thursday, March 25th, 5:00pm - 7:00pm',
     ],
     registrationURL: 'https://register.gotowebinar.com/rt/1942609860780894475',
   },
@@ -62,8 +62,21 @@ const webinars = [
     description: `Diagnostics and repair of the WM3570H*A/WM3770H*A LG Front Load
         Washer`,
     dates: [
-      'Wednesday, March 24th, 8:30am - 10:30am EST',
-      'Wednesday, March 24th, 8:30am - 10:30am EST',
+      'Wednesday, March 24th, 8:30am - 10:30am',
+      'Thursday, March 25th, 5:00pm - 7:00pm',
+    ],
+    registrationURL: 'https://register.gotowebinar.com/rt/1942609860780894475',
+  },
+  {
+    img: 'https://marketing-image-production.s3.amazonaws.com/uploads/523460e6cd6435290b821731c4b604ad15d94bee161ad919dc521778ce5287cea315d79b24efb8585331359fc369fa4800c0b176c977e4f0f6bb13eafffa35ad.png',
+    name: 'LG Front Load Washer WM3570H*A/WM3770H*A',
+    language: 'English',
+    host: 'Rick Kuemin',
+    description: `Diagnostics and repair of the WM3570H*A/WM3770H*A LG Front Load
+        Washer`,
+    dates: [
+      'Wednesday, March 24th, 8:30am - 10:30am',
+      'Thursday, March 25th, 5:00pm - 7:00pm',
     ],
     registrationURL: 'https://register.gotowebinar.com/rt/1942609860780894475',
   },
@@ -108,9 +121,11 @@ function renderContent(webinar) {
       <p class="webinar-description">
         ${webinar.description}
       </p>
-
-      <div class="webinar-dates">
-        ${dates}
+        <div class="webinar-dates__container">
+        <div class="webinar-dates__header bold">Session Times (EST)</div>
+        <div class="webinar-dates">
+            ${dates}
+        </div>
       </div>
       <a
         href="${webinar.registrationURL}"
