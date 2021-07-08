2b855f31148f02d8558476314215cd15e5b4ab5a
Ryan <ryanjay333@gmail.com>
2021-07-08 16:08:09 -0400

    added routes for admin post and US only filtering

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 // Components
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import WebinarGridContainer from './components/WebinarGridContainer';
 import Webinar from './components/Webinar';
 
 function App() {
@@ -25,13 +27,33 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+
       <div className="container">
         <Header />
-        <div className="webinar-grid" id="webinar-grid">
-          {webinars.map((webinar, i) => (
-            <Webinar webinar={webinar} key={i} />
-          ))}
-        </div>
+
+        <Route exact path="/">
+          <WebinarGridContainer>
+            {webinars.map((webinar, i) => (
+              <Webinar webinar={webinar} key={i} />
+            ))}
+          </WebinarGridContainer>
+        </Route>
+
+        {/* USA filter route */}
+        <Route exact path="/us">
+          <WebinarGridContainer>
+            {webinars
+              .filter((webinar) => webinar.country === 'USA')
+              .map((webinar, i) => (
+                <Webinar webinar={webinar} key={i} />
+              ))}
+          </WebinarGridContainer>
+        </Route>
+
+        {/* Admin route */}
+        <Route exact path="/postWebinar">
+          <h1>Post webinar</h1>
+        </Route>
       </div>
     </div>
   );
---
+++ client/src/components/WebinarGridContainer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function WebinarGridContainer({ children }) {
+  return <div className="webinar-grid">{children}</div>;
+}
+
+export default WebinarGridContainer;
